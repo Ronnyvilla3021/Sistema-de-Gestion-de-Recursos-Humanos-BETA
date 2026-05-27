29ef3bb631173c92e3547da1cd2af3df7ee65f2d
ronni2030 <rav.villa@yavirac.edu.ec>
2026-05-27 15:42:39 -0500

    run build para sitio web

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/Sistema-de-Gestion-de-Recursos-Humanos-Mini-HR-System-BETA-/',
   plugins: [react()],
 })
