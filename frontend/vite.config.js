98c048ea1b41655381551b583d3dc6ec2b256d67
ronni2030 <rav.villa@yavirac.edu.ec>
2026-05-27 15:48:25 -0500

    netfily

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/Sistema-de-Gestion-de-Recursos-Humanos-Mini-HR-System-BETA-/',
+  base: '/',
   plugins: [react()],
 })
